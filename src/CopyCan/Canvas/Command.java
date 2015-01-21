cebdd77fabf4f0349824e456a8919a58604ea3a1
Aconic <tygreman@gmail.com>
2015-01-21 09:48:36 +0200

    Code Files

--- src/CopyCan/Canvas/Command.java
+++ src/CopyCan/Canvas/Command.java
@@ -5,7 +5,6 @@ import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
-import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -31,7 +30,7 @@ public class Command
         public void actionPerformed(ActionEvent actionEvent)
         {
             data.col = JColorChooser.showDialog(null, "Select Color", data.col);
-          }
+        }
     }
 
     class ColorCommandRed implements ActionListener
@@ -93,13 +92,13 @@ public class Command
     }
 
 
-     class SliderCommand   implements ChangeListener
+    class SliderCommand implements ChangeListener
     {
         @Override
         public void stateChanged(ChangeEvent changeEvent)
         {
-          //  data.lw = wp.slider.getValue();
-        //    wp.labelW.setText(data.lw + " px");
+            //  data.lw = wp.slider.getValue();
+            //    wp.labelW.setText(data.lw + " px");
         }
     }
 }
