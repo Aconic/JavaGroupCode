ef6d807b4e50306ad3e5d3a480c7c96f71b8bb25
Aconic <tygreman@gmail.com>
2015-05-21 14:08:37 +0300

    Lesson 8.

---
+++ src/com/aconic/lessons/Lesson8/OddFrame.java
@@ -0,0 +1,15 @@
+package com.aconic.lessons.Lesson8;
+import javax.swing.JFrame;
+
+
+public class OddFrame extends JFrame
+{
+    public OddFrame()
+    {
+        setTitle("App for odd calculation");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBounds(200, 150, 500, 450);
+        add( new OddPanel() );
+        setVisible(true);
+    }
+}
---
+++ src/com/aconic/lessons/Lesson8/OddMain.java
@@ -0,0 +1,10 @@
+package com.aconic.lessons.Lesson8;
+
+
+public class OddMain
+{
+    public static void main(String[] args)
+    {
+        new OddFrame();
+    }
+}
