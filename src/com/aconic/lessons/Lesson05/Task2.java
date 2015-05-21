0cdd084aa6ebb4b1735b003983c97f74b6681fa0
Aconic <tygreman@gmail.com>
2015-05-21 15:43:33 +0300

    Lesson 5. Tasks

---
+++ src/com/aconic/lessons/Lesson05/Task2.java
@@ -0,0 +1,22 @@
+package com.aconic.lessons.Lesson05;
+
+public class Task2 {
+
+    public static  void main(String[] args)
+    {
+      int n = 10;
+
+      print(n);
+    }
+
+    public static void  print(int n)
+    {
+      for (int i = 1; i <=n; i++)
+      {
+          if(i % 2== 0)
+          {
+              System.out.print(i + " ");
+          }
+      }
+    }
+}
