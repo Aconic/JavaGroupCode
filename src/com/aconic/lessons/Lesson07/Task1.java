45c20236788bc22f6e736ccb3f78fb1fe01a9fb4
Aconic <tygreman@gmail.com>
2015-05-21 15:45:09 +0300

    Lesson 7. Tasks

---
+++ src/com/aconic/lessons/Lesson07/Task1.java
@@ -0,0 +1,35 @@
+package com.aconic.lessons.Lesson07;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+//Вывести дату прописью
+public class Task1
+{
+
+    public static void main(String[] args)
+    {
+        int day = 2;
+        int month = 3;
+        int year = 1;
+        System.out.print(getDate(day, month, year));
+
+    }
+
+    public static String getDate(int day, int month, int year)
+    {
+        String str = "";
+        str += Task1a.numString(day) + " ";
+        //str += toMonth(month) + " ";
+        str += Task1a.numString(year) + " год, ";
+        str += toWeekDay();      //алгоритм в интернете
+
+        return str;
+    }
+    public static String toWeekDay()
+    {
+        String res =  new SimpleDateFormat("EEEE").format(new Date()) ;
+        return res;
+    }
+}
---
+++ src/com/aconic/lessons/Lesson07/Task1a.java
@@ -0,0 +1,71 @@
+package com.aconic.lessons.Lesson07;
+
+import javax.swing.*;
+
+public class Task1a
+{
+
+
+    public static void main(String[] args)
+    {
+        int n = Integer.parseInt((JOptionPane.showInputDialog(null, "Введите число")));
+        JOptionPane.showMessageDialog(null, numString(n));
+    }
+
+    public static String numString(int n)
+    {
+        String[] strX = {"ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"};
+        String[] strXX = {"", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать",
+                "семнадцать", "восемнадцать", "девятнадцать"};
+        String[] strX10 = {"", "десять ", "двадцать ", "тридцать ", "сорок ", "пятьдесят ", "шестьдесят ", "семьдесят ",
+                "восемьдесят ", "девяносто "};
+        String[] strX100 = {"", "сто ", "двести ", "триста ", "четыреста ", "пятьсот ", "шестьсот ", "семьсот ",
+                "восемьсот ", "девятьсот "};
+        String res = "";
+        String space = " ";
+        if (n > 99 && n % 100 == 0 && n != 0)
+        {
+            res = strX100[n / 100];
+        }
+        else if (n == 99)
+        {
+            res = strX10[n / 10] + strX[n % 10];
+        }
+        else if (n > 99 && n % 10 == 0 && n != 0)
+        {
+            res = strX100[n / 100] + strX10[(n / 10 % 10)];
+        }
+        else if (n > 99 && n % 100 <= 9 && n != 0)
+        {
+            res = strX100[n / 100] + strX[(n % 10)];
+        }
+        else if (n > 99 && n / 10 % 10 < 2 && n != 0)
+        {
+            res = strX100[n / 100] + strXX[(n / 10 % 10)];
+        }
+        else if (n > 99 && n != 0)
+        {
+            res = strX100[n / 100] + strX10[(n / 10 % 10)] + strX[n % 10];
+        }
+        else if (n % 10 == 0 && n != 0 && n < 99)
+        {
+            res = strX10[n / 10];
+        }
+        else if (n % 10 != 0 && n > 20 && n < 99)
+        {
+            res = strX10[n / 10] + strX[n % 10];
+        }
+        else if (n > 10 && n < 20)
+        {
+            res = strXX[n / 10];
+        }
+        else if (n == 0 || n < 10)
+        {
+            res = strX[n];
+        }
+
+        return res;
+     }
+
+
+}
