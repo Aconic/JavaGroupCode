f7766c78bc8429de4f9b240d57ca906792f77354
Aconic <tygreman@gmail.com>
2015-05-21 14:27:39 +0300

    Chat (Java-Swing)

---
+++ src/com/aconic/apps/ChatSwing/ClientListener.java
@@ -0,0 +1,46 @@
+package com.aconic.apps.ChatSwing;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientListener extends Thread
+{
+    Socket cs;
+    DataInputStream in;
+    PanelChat panelChat;
+
+    public ClientListener(Socket socket, PanelChat panel) throws IOException
+    {
+        this.cs = socket;
+        this.panelChat = panel;
+    }
+
+    @Override
+    public void run()
+    {
+        try
+        {
+            in = new DataInputStream(cs.getInputStream());
+            while (true)                                      //listen to the server messages
+            {
+                String str = in.readUTF();
+                panelChat.textArea.append(str + "\n");
+            }
+
+        } catch (IOException e)
+        {
+            System.out.println("Client error");
+            e.printStackTrace();
+        } finally
+        {
+            try
+            {
+                in.close();
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/com/aconic/apps/ChatSwing/PanelChat.java
@@ -0,0 +1,86 @@
+package com.aconic.apps.ChatSwing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class PanelChat extends JPanel
+{
+    DataOutputStream out;
+    JTextField msgField;
+    JTextArea textArea;
+
+    public PanelChat() throws IOException
+    {
+        setLayout(null);
+        setBackground(Color.GRAY.brighter());
+        textArea = new JTextArea();
+
+        msgField = new JTextField();
+        msgField.setText("Enter your name");
+        JButton btnSend = new JButton("Send");
+
+        textArea.setBounds(10, 10, 300, 200);
+        msgField.setBounds(10, 220, 300, 40);
+        btnSend.setBounds(150, 300, 120, 30);
+
+        add(textArea);
+        add(msgField);
+        add(btnSend);
+
+        btnSend.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                try
+                {
+                    getClientText();
+                } catch (IOException e1)
+                {
+                    e1.printStackTrace();
+                }
+
+            }
+        });
+
+        msgField.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                try
+                {
+                    getClientText();
+                } catch (IOException e1)
+                {
+                    e1.printStackTrace();
+                }
+            }
+        });
+        startClient();
+    }
+
+
+    private void getClientText() throws IOException
+    {
+        out.writeUTF(msgField.getText());
+        msgField.setText("");
+    }
+
+
+    private void startClient() throws IOException
+    {
+        Socket cs = new Socket("LocalHost", 7777);
+        out = new DataOutputStream(cs.getOutputStream());
+        ClientListener cl = new ClientListener(new Socket("LocalHost", 7777), this);
+        cl.setDaemon(true);
+        cl.start();
+    }
+
+}
+
