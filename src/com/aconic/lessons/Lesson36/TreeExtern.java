bd259ef6728fdac78e24b9c15e764e2b09ead44c
Aconic <tygreman@gmail.com>
2015-05-21 14:24:34 +0300

    Lesson 36. Tree iterator

---
+++ src/com/aconic/lessons/Lesson36/TreeExtern.java
@@ -0,0 +1,34 @@
+package com.aconic.lessons.Lesson36;
+
+import java.util.Iterator;
+
+
+public class TreeExtern implements Iterator<Integer>
+{
+
+        int[] arr;
+        int index = 0;
+
+        public TreeExtern(int[] array)
+        {
+            arr = array;
+        }
+
+        @Override
+        public boolean hasNext ()
+        {
+            return index < arr.length;
+        }
+
+        @Override
+        public Integer next ()
+        {
+            return arr[index++];
+        }
+
+        @Override
+        public void remove ()
+        {
+        }
+
+    }
