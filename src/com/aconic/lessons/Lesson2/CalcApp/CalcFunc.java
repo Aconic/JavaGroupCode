cd87cc6aaf7575ef56d3341c08d48a00c1af43ab
Aconic <tygreman@gmail.com>
2015-05-21 14:36:35 +0300

    Lesson 2. Tasks

---
+++ src/com/aconic/lessons/Lesson2/CalcApp/CalcFunc.java
@@ -0,0 +1,27 @@
+package com.aconic.lessons.Lesson2.CalcApp;
+
+
+public class CalcFunc
+{
+        static int calc (int a, int b, String op)
+        {
+            int res = 0;
+            if (op.equals("+"))
+            {
+                res = a + b;
+            }
+            else if (op.equals( "-"))
+            {
+                res = a - b;
+            }
+            else if (op.equals("/"))
+            {
+                res = a/b;
+            }
+            else if (op.equals("*"))
+            {
+                res = a*b;
+            }
+            return res;
+        }
+}
---
+++ src/com/aconic/lessons/Lesson2/Task1.java
@@ -0,0 +1,39 @@
+package com.aconic.lessons.Lesson2;
+
+
+/**
+ * Определить к какой четверти принадлежит точка
+ */
+public class Task1 {
+    public static void main (String[] args)
+    {
+        int a = -34, b  = 160, fin = 0;
+
+        fin =  coord(a, b);
+    }
+
+    public static int coord(int x, int y)
+    {
+        int res= 0;
+        if(x > 0)
+        {
+            if(y >= 0)
+            {
+                res = 1;
+            }
+            res = 2;
+        }
+
+        else if (x < 0)
+        {
+            if(y >=0)
+            {
+                res = 4;
+            }
+            else res = 3;
+        }
+        return res;
+    }
+
+}
+
