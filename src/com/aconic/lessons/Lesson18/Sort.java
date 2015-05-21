0f1cd85a8e9ca648f8f8e93d37079b3a4205617c
Aconic <tygreman@gmail.com>
2015-05-21 14:37:35 +0300

    Lesson 18. Recursion

---
+++ src/com/aconic/lessons/Lesson18/Sort.java
@@ -0,0 +1,34 @@
+package com.aconic.lessons.Lesson18;
+
+import java.util.Arrays;
+
+public class Sort
+{
+    public static void main(String[] args)
+    {
+        int[] a = {2, 3, 6, 8};
+        int[] b = {1, 5, 10, 11};
+        int[] res = new int[a.length + b.length];
+        int a1 = 0;
+        int b1 = 0;
+        int c1 = 0;
+        while (a1 + b1 != res.length-1)
+        {
+            if (a[a1] < b[b1])
+            {
+             res[c1] = a[a1]; a1++;
+            }
+            else
+            { res[c1] = b[b1];
+
+            }
+            b1++;
+            c1++;
+        }
+
+
+    System.out.println(Arrays.toString(res));
+
+
+}
+}
