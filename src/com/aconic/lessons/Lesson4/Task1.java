4a46679e791d84d213503be098b022f9c93ab300
Aconic <tygreman@gmail.com>
2015-05-21 14:47:57 +0300

    Lesson 4. Task

---
+++ src/com/aconic/lessons/Lesson4/Task1.java
@@ -0,0 +1,16 @@
+package com.aconic.lessons.Lesson4;
+
+/**
+Вывести таблицу ASCII
+ */
+public class Task1 {
+
+    public static void main (String[]args)
+    {
+        for (char i = 32; i <= 127; i++)
+        {
+            System.out.print(i + " ");
+
+        }
+    }
+}
