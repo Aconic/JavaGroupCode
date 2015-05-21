64c6d01ca5a5cbb3fac2918944ab17b94f34b91b
Aconic <tygreman@gmail.com>
2015-05-21 14:45:30 +0300

    Paint. Classwork

---
+++ src/com/aconic/apps/CanvasClassEdition/Main.java
@@ -0,0 +1,18 @@
+package com.aconic.apps.CanvasClassEdition;
+
+
+import javax.swing.*;
+
+public class Main
+{
+    public static void main(String[]args)
+    {
+        try
+        {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+        }
+        catch(Exception e){
+        }
+       new Frame();
+    }
+}
